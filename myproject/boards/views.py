28bfac712c3e26729494b50456364d14fd7200f8
mike840609 <mike840609@gmail.com>
2017-11-07 20:25:24 +0800

    css && static file

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from .models import Board
 from django.http import HttpResponse
 
+
 def home(request):
-    return HttpResponse('Hello, World!')
+    # return HttpResponse('Hello, World!')
+
+    # boards = Board.objects.all()
+    # boards_names = list()
+
+    # for board in boards:
+    #     boards_names.append(board.name)
+
+    # response_html = '<br>'.join(boards_names)
+    # return HttpResponse(response_html)
+
+    boards = Board.objects.all()
+    return render(request , 'home.html', {'boards':boards})
+
+
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -36,9 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    
+    # css file
     'django.contrib.staticfiles',
 
-     'boards',
+    'boards',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,11 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR , 'templates')
+            
+
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
